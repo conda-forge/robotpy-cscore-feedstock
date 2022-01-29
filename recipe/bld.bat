7b7a5f9254ddda6d6a75526431fdbc79780b279b
David Vo <auscompgeek@users.noreply.github.com>
2022-01-29 21:57:41 +1100

    Avoid building PortForwarder on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,9 @@ setlocal
 rem Use conda-forge pybind11
 del /s /q pybind11
 
+rem Workaround C++20 breakage
+del cscore_src\wpiutil\src\main\native\cpp\PortForwarder.cpp
+
 set CL=/I%LIBRARY_PREFIX%\include
 set LIB=%LIBRARY_PREFIX%\lib;%LIB%
 
