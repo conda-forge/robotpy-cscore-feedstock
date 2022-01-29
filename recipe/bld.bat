f5479f64081d30371cb1e427056851281afe9c1c
David Vo <auscompgeek@users.noreply.github.com>
2022-01-29 22:39:14 +1100

    Avoid building JNI on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,10 @@ del /s /q pybind11
 rem Workaround C++20 breakage
 del cscore_src\wpiutil\src\main\native\cpp\PortForwarder.cpp
 
+rem Ignore inadvertently picked up JNI stuff
+del cscore_src\cscore\src\main\native\cpp\jni\CameraServerJNI.cpp
+del cscore_src\wpiutil\src\main\native\cpp\jni\WPIUtilJNI.cpp
+
 set CL=/I%LIBRARY_PREFIX%\include
 set LIB=%LIBRARY_PREFIX%\lib;%LIB%
 
